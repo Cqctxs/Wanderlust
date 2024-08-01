55946e131980110a6efb73f65b1818d98da31ca8
Felix Zhao <zhaofelixdelt@gmail.com>
2024-07-31 23:48:13 -0400

    frame for home page

---
+++ src/components/ui/navbar/frame.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import topRight from "@/assets/top_right.svg";
+import topLeft from "@/assets/top_left.svg";
+import bottomRight from "@/assets/bottom_right.svg";
+import bottomLeft from "@/assets/bottom_left.svg";
+
+export const Frame = () => {
+  return (
+    <div>
+      <div className="absolute h-full w-6 bg-wh" />
+      <div className="absolute right-0 h-full w-6 bg-wh" />
+      <div className="absolute h-6 w-full bg-wh" />
+      <div className="absolute bottom-0 h-6 w-full bg-wh" />
+      <div className="absolute top-6 left-6 bg-[url('../assets/top_left.svg')] bg-no-repeat h-20 w-80" />
+      <div className="absolute top-6 right-6 bg-[url('../assets/top_right.svg')] bg-no-repeat h-20 w-80" />
+      <div className="absolute bottom-6 left-6 bg-[url('../assets/bottom_left.svg')] bg-no-repeat h-20 w-80" />
+      <div className="absolute bottom-6 right-6 bg-[url('../assets/bottom_right.svg')] bg-no-repeat h-20 w-80" />
+    </div>
+  );
+};
