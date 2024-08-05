7acc543a8819765e47fba330feb146d46a386373
Felix Zhao <zhaofelixdelt@gmail.com>
2024-08-05 17:50:03 -0400

    white frame

--- src/components/ui/navbar/frame.jsx
+++ src/components/ui/navbar/frame.jsx
@@ -8,11 +8,11 @@ export const Frame = () => {
   return (
     <>
       {/* Background and title */}
-      <div className="  z-50 drop-shadow-or absolute inset-0">
-        <div className="z-0 absolute h-full w-6 bg-or" />
-        <div className="z-0 absolute right-0 h-full w-6 bg-or" />
-        <div className="z-0 absolute h-6 w-full bg-or" />
-        <div className="z-0 absolute bottom-0 h-6 w-full bg-or" />
+      <div className="  z-50 drop-shadow-wh absolute inset-0">
+        <div className="z-0 absolute h-full w-6 bg-wh" />
+        <div className="z-0 absolute right-0 h-full w-6 bg-wh" />
+        <div className="z-0 absolute h-6 w-full bg-wh" />
+        <div className="z-0 absolute bottom-0 h-6 w-full bg-wh" />
         <div className="z-0 absolute top-6 left-6 bg-[url('/assets/top_left.svg')] bg-no-repeat h-20 w-80" />
         <div className="z-0 absolute top-6 right-6 bg-[url('/assets/top_right.svg')] bg-no-repeat h-20 w-80" />
         <div className="z-0 absolute bottom-6 left-6 bg-[url('/assets/bottom_left.svg')] bg-no-repeat h-20 w-80" />
@@ -25,7 +25,7 @@ export const Frame = () => {
           </button>
         </Link>
         <Link href="/">
-          <button className="absolute font-offbit top-1 left-4 text-[3.5rem] text-bl drop-shadow-wh">wanderlust</button>
+          <button className="absolute font-offbit top-1 left-4 text-[3.5rem] text-or drop-shadow-or">wanderlust</button>
         </Link>
       </div>
     </>
