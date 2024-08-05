683583430b622a109e9d454939d30568e7f3ca16
Felix Zhao <zhaofelixdelt@gmail.com>
2024-08-05 16:53:08 -0400

    map rotation

--- src/app/map/page.jsx
+++ src/app/map/page.jsx
@@ -7,18 +7,22 @@ const TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 const INITIAL_VIEW_STATE = {
     latitude: 43.642567,
     longitude: -79.387054,
-    zoom: 11,
+    zoom: 15,
     bearing: 0,
     pitch: 0
 };
 
 export default function MapPage() {
     return (
-        <DeckGL
-            initialViewState={INITIAL_VIEW_STATE}
-            controller={true}
-        >
-            <Map mapStyle="mapbox://styles/mapbox/standard-beta" mapboxAccessToken={TOKEN} />
-        </DeckGL>
+        <div onContextMenu={(e) => {
+            e.preventDefault();
+        }}>
+            <DeckGL
+                initialViewState={INITIAL_VIEW_STATE}
+                controller={true}
+            >
+                <Map mapStyle="mapbox://styles/mapbox/standard-beta" mapboxAccessToken={TOKEN} />
+            </DeckGL>
+        </div>
     );
 }
