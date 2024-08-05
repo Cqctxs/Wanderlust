7bed50d8a23e35bc0be63727daac39324b28e4d2
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-08-05 18:44:47 -0400

    trying to connect frontend to backend

--- src/app/plan/page.jsx
+++ src/app/plan/page.jsx
@@ -1,30 +1,31 @@
+'use client'
+
 import React from 'react'
 import { QueryClient, useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 const Plan = () => {
 
-  const {} = useQuery({
+  const {data, isPending, isError, error} = useQuery({
     queryKey: ["location"],
     queryFn: async () => {
-      const { data } = await axios.get("localhost:8080/api/generate", {
-        params: {
-          
-          country: "United States",
-          startDate: "2024-10-10",
-          endDate: "2024-10-15",
-          sub: "asdfadfadf" 
-        }
+      const {data} = await axios.get("http://localhost:8080/api/generate", {
+        "country": "United States",
+        "startDate": "2024-10-10",
+        "endDate": "2024-10-15",
+        "sub": "asdfadfadf"
       }).then((res) => console.log(res))
       .catch((error) => console.log(error));
+      return data;
     },
   });
 
+  if (isPending) return <div>Loading tasks...</div>;
+  if (isError) return <div>Error: {error.message}</div>
+
   return (
     <div className="flex-col items-center h-full justify-center align-center">
-        <h1 className="flex justify-center">Where do you want to visit?</h1>
-        <form action="localhost:8080/api/generate" method="GET">
-        
-        </form>
+        <h1 className="flex justify-center">{JSON.stringify(data, null, 2)}</h1>
     </div>
   )
 }
