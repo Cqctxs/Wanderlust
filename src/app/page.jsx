791b6c7b9cd70ab14bf92eabed1aed17ab724227
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-05 23:15:16 -0400

    finished parallax. it's beautiful.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,9 +11,11 @@ export default function Home() {
     <div>
       <Frame/>
       <CityParallax 
-        sky_0={"/assets/citybg_day_0.png"} sky_1={"/assets/citybg_day_1.png"} sky_2={"/assets/citybg_day_2.png"}
-        city_0={"/assets/citybg_blue_0.png"} city_1={"/assets/citybg_blue_1.png"} city_2={"/assets/citybg_blue_2.png"} city_3={"/assets/citybg_blue_3.png"} 
+        sky_0={"/assets/citybg_sunset_0.png"} sky_1={"/assets/citybg_sunset_1.png"} sky_2={"/assets/citybg_sunset_2.png"}
+        city_0={"/assets/citybg_orange_0.png"} city_1={"/assets/citybg_orange_1.png"} city_2={"/assets/citybg_orange_2.png"} city_3={"/assets/citybg_orange_3.png"} 
       ></CityParallax>
+      <div>
+      </div>
     </div>
   );
 }
--- src/components/ui/navbar/frame.jsx
+++ src/components/ui/navbar/frame.jsx
@@ -10,18 +10,18 @@ export const Frame = () => {
       <div className="fixed top-0 left-0 right-0 z-50 bg-white drop-shadow-md">
       {/* Background and title */}
       <div className="relative h-20 bg-white">
-        <div className="absolute inset-0 bg-white"></div>
-        <div className="absolute top-0 left-0 bg-[url('/assets/top_left.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
-        <div className="absolute top-0 right-0 bg-[url('/assets/top_right.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
+        <div className="absolute inset-0 w-full h-8 bg-wh"></div>
+        <div className="absolute top-8 left-0 bg-[url('/assets/top_left.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
+        <div className="absolute top-8 right-0 bg-[url('/assets/top_right.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
 
         {/* Logo */}
         <Link href="/">
-          <div className="absolute top-0 left-4 bg-[url('/assets/logoWide.png')] bg-no-repeat bg-contain h-10 w-80"></div>
+          <div className="absolute top-6 left-4 bg-[url('/assets/logoWide.png')] bg-no-repeat bg-contain h-20 w-60"></div>
         </Link>
 
         {/* Navigation Button */}
         <Link href="/map">
-          <button className="absolute top-3 right-6 flex items-center space-x-2">
+          <button className="absolute top-6 right-10 flex items-center space-x-2">
             <Globes start={0} />
             <Globes start={1} />
             <Globes start={2} />
