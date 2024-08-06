c7106b1ad50626edebdfa2c1de84b54162856f6a
Sean <seanzhao2007@gmail.com>
2024-08-05 20:42:59 -0400

    Fixed the data fetching

--- src/app/plan/page.jsx
+++ src/app/plan/page.jsx
@@ -1,37 +1,35 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { QueryClient, useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Plan = () => {
+  const fetchItenerary = async (country, startDate, endDate, sub) => {
+    const res = await axios.post("http://localhost:8080/api/generate", {
+      country,
+      startDate,
+      endDate,
+      sub,
+    }, {
+      headers: { "Content-Type": "application/json" },
+    });
+    return res.data; // Return the data from the response
+  };
 
-  const country = "Japan";
-  const startDate = "2024-10-10";
-  const endDate = "2024-10-15";
-
-  const {data, isPending, isError, error} = useQuery({
-    queryKey: ["location"],
-    queryFn: async () => {
-      const {data} = await axios.post("http://localhost:8080/api/generate",
-        JSON.stringify({country, startDate, endDate}),
-        {
-            headers: { 'Content-Type': 'application/json' },
-        }
-    ).then((res) => console.log(res))
-      .catch((error) => console.log(error));
-      return data;
-    },
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["itenerary"],
+    queryFn: () => fetchItenerary("China", "2024-10-10", "2024-10-15", "asdfadfadf"),
   });
 
   if (isPending) return <div>Loading tasks...</div>;
-  if (isError) return <div>Error: {error.message}</div>
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <div className="flex-col items-center h-full justify-center align-center">
-        <h1 className="flex justify-center">{data}</h1>
+      <h1 className="flex justify-center">{data}</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Plan;
