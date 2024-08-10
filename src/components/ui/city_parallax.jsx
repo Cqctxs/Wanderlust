3a770b0be978b9cf30bba8c5855f82ae0508ad3d
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-08-10 17:10:03 -0400

    global.css file

--- src/components/ui/city_parallax.jsx
+++ src/components/ui/city_parallax.jsx
@@ -2,12 +2,21 @@
 
 import React, { useEffect, useState } from 'react';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import Head from 'next/head';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const CityParallax = ({ sky_0, sky_1, sky_2, city_0, city_1, city_2, city_3, everything_after }) => {
   const [isClient, setIsClient] = useState(false);
 
+
   useEffect(() => {
     setIsClient(true);
+    // initializes animate on scroll
+    AOS.init({
+        duration: 1200, 
+        once: true,
+    });
   }, []);
 
   if (!isClient) {
@@ -15,33 +24,38 @@ export const CityParallax = ({ sky_0, sky_1, sky_2, city_0, city_1, city_2, city
   }
 
   return (
+    <>
+    <Head>
+        {/* external stylesheets */}
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    </Head>
     <div>
-      <style>
+    <style>
         {`
-          #textblock {
+        #textblock {
             background-color: #2a2320;
             height: 100vh;
-          }
+        }
 
-          #textblock-container {
+        #textblock-container {
             width: 50%;
             margin: 0 auto;
             padding-top: 70px;
-          }
+        }
 
-          #textblock-title {
+        #textblock-title {
             color: #ffaf1b;
             font-size: 35px;
             font-weight: 600;
             font-family: "Helvetica Neue";
-          }
+        }
 
-          #textblock-content {
+        #textblock-content {
             color: #ffaf1b;
             font-size: 20px;
-          }
+        }
 
-          #textblock-footer {
+        #textblock-footer {
             color: #ffaf1b;
             font-size: 15px;
             font-weight: 400;
@@ -52,133 +66,134 @@ export const CityParallax = ({ sky_0, sky_1, sky_2, city_0, city_1, city_2, city
             align-items: center;
             text-align: center;
             margin-bottom: 20px;
-          }
+        }
 
-          #textblock-devsense {
+        #textblock-devsense {
             text-decoration: none;
             color: #ffaf1b;
             font-size: 15px;
             font-weight: 600;
-          }
+        }
 
-          .animation, .animation_layer {
+        .animation, .animation_layer {
             height: 1000px;
-          }
+        }
 
-          .animation {
+        .animation {
             display: block;
             position: relative;
             z-index: 10;
-          }
+        }
 
-          .animation_layer {
+        .animation_layer {
             background-position: bottom center;
             background-size: cover;
             background-repeat: no-repeat;
             width: 100%;
             position: absolute;
-          }
-          
-          .logo_layer {
+        }
+        
+        .logo_layer {
             background-position: center;
             background-repeat: no-repeat;
             background-size: cover;
             width: 1136px;
             height: 184px;
-          }
+        }
 
-          .animation_layer.parallax {
+        .animation_layer.parallax {
             position: fixed;
-          }
+        }
 
-          .gradient-text:hover {
+        .gradient-text:hover {
             background: linear-gradient(to right, #357cfe, #e74c5c);
             -webkit-background-clip: text;
             -webkit-text-fill-color: transparent;
-          }
+        }
 
-          .gradient-title {
+        .gradient-title {
             background: linear-gradient(to right, #ffaf1b, #cc6f0b);
             -webkit-background-clip: text;
             -webkit-text-fill-color: transparent;
-          }
+        }
 
-          #artback {
+        #artback {
             background-image: url("/assets/citybg_sunset_2.png");
-          }
+        }
 
-          #mountain {
+        #mountain {
             background-image: url("/assets/citybg_sunset_1.png");
-          }
+        }
 
-          #logoland {
+        #logoland {
             background-image: url("/assets/citybg_sunset_0.png");
-          }
+        }
 
-          #jungle1 {
+        #jungle1 {
             background-image: url("/assets/citybg_orange_3.png");
-          }
+        }
 
-          #jungle2 {
+        #jungle2 {
             background-image: url("/assets/citybg_orange_2.png");
-          }
+        }
 
-          #jungle3 {
+        #jungle3 {
             background-image: url("/assets/citybg_orange_1.png");
-          }
+        }
 
-          #jungle4 {
+        #jungle4 {
             background-image: url("/assets/citybg_orange_0.png");
-          }
+        }
         `}
-      </style>
-      <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
+    </style>
+    <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
         <ParallaxLayer offset={0} speed={0.25}>
-          <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_sunset_2.png)" }}></div>
+        <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_sunset_2.png)" }}></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={-0.2}>
-          <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_sunset_1.png)" }}></div>
+        <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_sunset_1.png)" }}></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={-0.1}>
-          <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_sunset_0.png)" }}></div>
+        <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_sunset_0.png)" }}></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.05}>
-          <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_orange_3.png)" }}></div>
+        <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_orange_3.png)" }}></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.15}>
-          <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_orange_2.png)" }}></div>
+        <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_orange_2.png)" }}></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.25}>
-          <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_orange_1.png)" }}></div>
+        <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/citybg_orange_1.png)" }}></div>
         </ParallaxLayer>
         {/* logo */}
         <ParallaxLayer offset={0} speed={-1.1}>
-          <div className="w-full absolute h-auto parallax mt-40 flex justify-center">
+        <div className="w-full absolute h-auto parallax mt-40 flex justify-center">
             <h1 className="m-0 p-2 rounded-lg text-center text-shadow-xl font-offbit text-9xl text-or
-                           backdrop-brightness-125 backdrop-blur-xl">wanderlust</h1>
-          </div>
+                        backdrop-brightness-125 backdrop-blur-xl">wanderlust</h1>
+        </div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.3}>
-          <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/darkest.png)" }}></div>
+        <div className="animation_layer parallax" style={{ backgroundImage: "url(/assets/darkest.png)" }}></div>
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0.3}>
-          <div className="h-full bg-[#2a2320]">
+        <div className="h-full bg-[#2a2320]">
             <div className="m-auto p-[70] w-full">
-              <h1 className="gradient-title font-sans font-bold text-6xl w-full text-center text-shadow-xl">Wanderlust</h1>
-              <div className="flex items-center justify-evenly mt-8">
+            <h1 className="gradient-title font-sans font-bold text-6xl w-full text-center text-shadow-xl" data-aos="fade-down">Wanderlust</h1>
+            <div className="flex items-center justify-evenly mt-8">
                 <p className="text-4xl font-sans text-home w-[40%] text-center">
                     Powered by <a className="font-bold text-home-dark gradient-text" href="https://gemini.google.com/">Google's Gemini AI</a>, 
                     Wanderlust uses a <b className="highlight highlight-variant-4 highlight-[#1b2e57]">large database</b> of popular
                     <b className="highlight highlight-variant-4 highlight-[#405a53]"> tourism destinations</b> to tailor your travel itinerary.
                 </p>
                 <img className="drop-shadow-xl rounded-2xl w-[40%] h-auto" src="/assets/blank_map.png"></img>
-              </div>
+            </div>
                 
             </div>
-          </div>
+        </div>
         </ParallaxLayer>
-      </Parallax>
+    </Parallax>
     </div>
+    </>
   );
 };
 
