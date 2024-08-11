f179933ea3b8538037484251a3fd38efa5539396
Avery Lee <itsaveryleee@gmail.com>
2024-08-11 00:21:13 -0400

    dsfasdf

--- src/components/ui/navbar/frame.jsx
+++ src/components/ui/navbar/frame.jsx
@@ -9,7 +9,7 @@ export const Frame = () => {
     <>
       <div className="fixed top-0 left-0 right-0 z-10 bg-white drop-shadow-md">
       {/* Background and title */}
-      <div>
+      <div> 
         <div className="absolute inset-0 w-full h-8 bg-wh"></div>
         <div className="absolute top-8 left-0 bg-[url('/assets/top_left.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
         <div className="absolute top-8 right-0 bg-[url('/assets/top_right.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
