d124fdb7437413bcc3a3f0d65361c3d7b02056b9
Avery Lee <itsaveryleee@gmail.com>
2024-08-10 22:40:00 -0400

    committt

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,17 +19,17 @@ export default function Home() {
         everything_after={
           <div className="h-full bg-[#252221]">
             <div className="pt-20 w-full">
-                <div className="flex rounded-lg items-center justify-evenly mt-8 m-14 bg-[#353130] py-16 px-16" data-aos="fade-right">
-                    <p className="text-4xl font-sans text-wh w-2/5 px-2 text-right leading-snug">
+                <div className="flex rounded-xl drop-shadow-xl items-center justify-evenly mt-8 ml-32 mr-32 bg-[#353130]" data-aos="fade-right">
+                    <p className="text-4xl font-sans text-wh w-2/5 pr-4 text-left ml-20  leading-tight">
                         Powered by <a className="text-blu gradient-text" href="https://gemini.google.com/">Gemini AI</a>, 
                         Wanderlust uses a large database of popular tourism destinations to tailor your travel itinerary. <br /><br />
-                        <div className="flex rounded-xl bg-blu justify-center py-8">
+                        <div className="flex  rounded-lg bg-blu justify-center py-6 mr-24">
                           <a href="/plan">
-                            <h1 className="font-sans text-wh font-extrabold text-7xl">Get Started     </h1>
+                            <h1 className="font-sans text-wh font-extrabold text-5xl">Get started     </h1>
                           </a>
                         </div>
                     </p>
-                    <img className="drop-shadow-xl rounded-[2%] w-3/5 ml-10 h-auto" src="/assets/tripexample.png" alt="Trip Example"></img>
+                    <img className="w-[55%] rounded-r-xl ml-10 h-max" src="/assets/tripexample.png" alt="Trip Example"></img>
                 </div>
             </div>
           </div>
