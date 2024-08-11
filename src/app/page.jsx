d52c5177a6b459653bbfaf42951f3bb497fc7a67
Avery Lee <itsaveryleee@gmail.com>
2024-08-10 22:38:51 -0400

    suhdaidhfwdkljfhlwekjfhale

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,21 +18,21 @@ export default function Home() {
         searchValue={<></>}
         everything_after={
           <div className="h-full bg-[#252221]">
-    <div className="pt-20 w-full">
-        <div className="flex rounded-lg items-center justify-evenly mt-8 m-14 bg-[#353130] py-16 px-16" data-aos="fade-right">
-            <p className="text-4xl font-sans text-wh w-2/5 px-2 text-right leading-snug">
-                Powered by <a className="text-blu gradient-text" href="https://gemini.google.com/">Gemini AI</a>, 
-                Wanderlust uses a large database of popular tourism destinations to tailor your travel itinerary. <br /><br />
-                <div className="flex rounded-xl bg-blu justify-center py-8">
-                  <a href="/plan">
-                    <h1 className="font-sans text-wh font-extrabold text-7xl">Get Started     </h1>
-                  </a>
+            <div className="pt-20 w-full">
+                <div className="flex rounded-lg items-center justify-evenly mt-8 m-14 bg-[#353130] py-16 px-16" data-aos="fade-right">
+                    <p className="text-4xl font-sans text-wh w-2/5 px-2 text-right leading-snug">
+                        Powered by <a className="text-blu gradient-text" href="https://gemini.google.com/">Gemini AI</a>, 
+                        Wanderlust uses a large database of popular tourism destinations to tailor your travel itinerary. <br /><br />
+                        <div className="flex rounded-xl bg-blu justify-center py-8">
+                          <a href="/plan">
+                            <h1 className="font-sans text-wh font-extrabold text-7xl">Get Started     </h1>
+                          </a>
+                        </div>
+                    </p>
+                    <img className="drop-shadow-xl rounded-[2%] w-3/5 ml-10 h-auto" src="/assets/tripexample.png" alt="Trip Example"></img>
                 </div>
-            </p>
-            <img className="drop-shadow-xl rounded-[2%] w-3/5 ml-10 h-auto" src="/assets/tripexample.png" alt="Trip Example"></img>
-        </div>
-    </div>
-  </div>
+            </div>
+          </div>
 
         }
       ></CityParallax>
