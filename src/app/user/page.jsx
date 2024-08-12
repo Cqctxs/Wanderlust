6d0a926ca85ebd858cde114a2fa86615b34f0adf
eogito <glenlin7813@gmail.com>
2024-08-12 11:38:47 -0400

    fixed merge editor

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,15 +1,28 @@
 "use client";
 
+import React from 'react';
+import { DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Box } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
 import { Frame } from "@/components/ui/navbar/frame";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { UserRoundX } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import Footer from "@/components/ui/navbar/footer";
+import { MapSmall } from '@/components/ui/mapsmall';
 import axios from "axios";
 
 export default function ProfileClient() {
   const { user, isLoading } = useUser();
+  const [open, setOpen] = React.useState(false);
 
+
+  const handleClickToOpen = () => {
+    setOpen(true);
+  };
+
+  const handleToClose = (value) => {
+    setOpen(false);
+  };
   const fetchPreviousGenerations = async (sub) => {
     const res = await axios.post(
       "https://portfolio-backend-430914.nn.r.appspot.com/api/user",
@@ -98,31 +111,92 @@ export default function ProfileClient() {
             </div>
           </div>
 
-          {/* Right Column */}
           <div className="col-span-3 bg-[#F7F5F2] rounded-lg shadow-md overflow-hidden p-6 flex flex-col h-full">
-            <h2 className="text-2xl font-semibold text-center text-[#252221] mb-2">
+            <h2 className="text-2xl font-semibold text-center text-[#252221] mb-4">
               Miscellaneous Information
             </h2>
             <div className="space-y-4 flex-grow">
               {isPending ? (
                 <p className="text-gray-600">Loading...</p>
-              ) : error && !data ? (
-                <p>Could not find any previous data. Try generating a trip!</p>
               ) : (
-                <div className="text-gray-600">
+                (error && !data) ? (
+                  <p>Could not find any previous data. Try generating a trip!</p>
+                ) : (
+                  <div className="text-gray-600">
                   {data.previousGenerations.map((travelPlan, index) => (
                     <div key={index} className="mb-4">
-                      <h2 className="font-bold">{travelPlan.country}</h2>
-                      <p>
-                        {travelPlan.itinerary[0].date} to{" "}
-                        {
-                          travelPlan.itinerary[travelPlan.itinerary.length - 1]
-                            .date
-                        }
-                      </p>
+                      <div className="grid gap-4 grid-cols-2">
+                        <div>
+                          <h2 className="font-bold">
+                            {travelPlan.country}
+                          </h2>
+                          <p>{travelPlan.itinerary[0].date} to {travelPlan.itinerary[travelPlan.itinerary.length - 1].date}</p>
+                        </div>
+                        
+                        <div className="flex justify-end">
+                          <a 
+                            className="bg-blu text-wh font-sans text-1xl font-bold py-3 px-5 rounded shadow-md transition-transform hover:scale-105"
+                            onClick={handleClickToOpen}
+                          >
+                            Review This Trip
+                          </a>
+                        </div>
+                      </div>
+                      
+                      <hr style={{width: "100%"}}></hr>
+                      <Box>
+                          <Dialog open={open} onClose={handleToClose} fullScreen={true}>
+                            <DialogContent>
+                              <div className="h-full bg-[#252322] flex flex-col justify-center items-center px-8 py-16 pb-40">
+                                <h1 className="text-wh font-sans text-8xl mb-12">Your trip to {travelPlan?.country}</h1>
+                                <div className="flex h-min w-screen mx-8 px-12 space-x-8">
+                                  <div className="relative flex w-3/5 bg-[#353130] rounded-2xl shadow-lg overflow-hidden">
+                                    <MapSmall data={travelPlan} />
+                                  </div>
+                                  <div className="flex flex-col w-2/5 space-y-4">
+                                    <div className="relative bg-[#353130] rounded-2xl shadow-lg p-8 pb-8 flex flex-col overflow-y-auto h-[500px]">
+                                      <div className="text-wh text-lg flex-1">
+                                        <h1 className="text-wh font-sans text-4xl mb-2 text-center">Travel Itinerary</h1>
+                                        <ul className="list-disc ml-4 space-y-2 text-2xl text-[#c3c0c0]">
+                                          {travelPlan?.itinerary.map((day, index) => (
+                                            <li key={index}>
+                                              {day.city} - {day.date}
+                                              <ul className="list-disc ml-8 mt-2 text-lg font-extralight text-[#c3c0c0]">
+                                                {day.locations.map((activity, i) => (
+                                                  <li key={i}>{activity}</li>
+                                                ))}
+                                              </ul>
+                                            </li>
+                                          ))}
+                                        </ul>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </DialogContent>
+                            <DialogActions>
+                              <Button onClick={handleToClose}
+                                color="primary" autoFocus>
+                                  Close
+                                </Button>
+                            </DialogActions>
+                          </Dialog>
+                        </Box>
+                      {/* {travelPlan?.itinerary.map((day, index) => (
+                        <li key={index}>
+                          {day.city} - {day.date}3
+                          <ul className="list-disc ml-8 mt-2 text-lg font-extralight text-[#c3c0c0]">
+                            {day.locations.map((activity, i) => (
+                              <li key={i}>{activity}</li>
+                            ))}
+                          </ul>
+                        </li>
+                      ))} */}
                     </div>
                   ))}
-                </div>
+                  </div>
+                )
               )}
             </div>
           </div>
