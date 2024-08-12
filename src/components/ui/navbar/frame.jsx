e454b20333fd6c2422a2e034f45fa9c21a4d6648
Avery Lee <itsaveryleee@gmail.com>
2024-08-12 02:53:24 -0400

    jhvjhgvjhg

--- src/components/ui/navbar/frame.jsx
+++ src/components/ui/navbar/frame.jsx
@@ -11,22 +11,23 @@ export const Frame = () => {
       {/* Background and title */}
       <div> 
         <div className="absolute inset-0 w-full h-8 bg-wh"></div>
-        <div className="absolute top-8 left-0 bg-[url('/assets/top_left.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
-        <div className="absolute top-8 right-0 bg-[url('/assets/top_right.svg')] bg-no-repeat bg-contain h-20 w-80"></div>
+        <div className="absolute top-8 left-0 bg-[url('/assets/top_left.png')] bg-no-repeat bg-contain h-20 w-80"></div>
+        <div className="absolute top-8 right-0 bg-[url('/assets/top_right.png')] bg-no-repeat bg-contain h-20 w-80"></div>
 
         {/* Logo */}
         <Link href="/">
-          <div className="absolute top-6 left-4 bg-[url('/assets/logoWide.png')] bg-no-repeat bg-contain h-20 w-60"></div>
+          <div className="absolute top-7 left-4 bg-[url('/assets/logoWide.png')] bg-no-repeat bg-contain h-20 w-60"></div>
         </Link>
 
         {/* Navigation Button */}
         <Link href="/map">
-          <button className="absolute top-6 right-10 flex items-center space-x-2">
+          <button className="absolute top-4 right-[86px] flex items-center space-x-3">
             <Globes start={0} />
             <Globes start={1} />
             <Globes start={2} />
           </button>
         </Link>
+        <div className="absolute top-3 -right-40 bg-[url('/assets/logoIcon.png')] bg-no-repeat bg-contain h-16 w-60"></div>
       </div>
     </div>
     </>
